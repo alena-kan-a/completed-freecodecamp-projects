e5335cec04f72947290004276c142f67576781e3
Alena K <alena.kandazhenka@gmail.com>
2024-02-26 17:29:00 +0900

    Updated script (in progress)

--- palindrome-checker/script.js
+++ palindrome-checker/script.js
@@ -3,12 +3,31 @@ const textInput = document.getElementById("text-input");
 const resultOutput = document.getElementById("result");
 
 function cleanInputString(str) {
-const regex = /\s/g;
-return str.replace(regex, '');
+  const regex = /\s\d\W/gi;
+  return str.replace(regex, '').toLowerCase();
 };
 
-function checkWord(word) {
-
+function checkWord (word) {
+  const wordToCheck = word;
+  
+  if (word === '') {
+    alert('Please input a value');
+    return;
+  }
+  
+  const cleanString = cleanInputString(word);
+  const reverseWord = [...cleanString].reverse().join('');
+  if (cleanString === reverseWord) {
+    const HTMLString = `<p>Your word <span class="emphasized-text">${wordToCheck}</span> is a palindrome. Hooray!</p>`;
+    resultOutput.classList.remove('hide');
+    return resultOutput.innerHTML = HTMLString;
+  } else if (cleanString !== reverseWord) {
+    const HTMLString = `<p>Your word <span class="emphasized-text">${wordToCheck}</span> is not a palindrome. Try again!</p>`;
+    resultOutput.classList.remove('hide');
+    return resultOutput.innerHTML = HTMLString;
+  };
 };
 
-checkWordButton.addEventListener("submit", checkWord);
+checkWordButton.addEventListener("click", () => {
+    checkWord(textInput.value);
+});
